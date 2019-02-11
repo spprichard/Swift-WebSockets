eb25862c0575d2737cd756214c1c6590ed1cc163
spprichard <spprichard20@gmail.com>
2019-02-11 08:12:32 -0600

    got websocket working, prints out things read from socket

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,9 +2,5 @@ import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-//    let conf = NIOServerConfig.default(hostname: "ws-feed.pro.coinbase.com")
-//    services.register(conf)
-//
-//    let wss = NIOWebSocketServer.default()
-//    services.register(wss, as: WebSocketServer.self)
+    // configureation stuff here
 }
--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,39 +1,48 @@
+//
+//  main.swift
+//  App
+//
+//  Created by Steven Prichard on 2/10/19.
+//
+
 import App
+import Service
 import Vapor
 
-public struct Subscribe: Encodable {
-    var type: String = "subscribe"
-    var channels: [String] = [
-        "level2"
-    ]
-    var product_ids: [String] = [
-        "BTC-USD"
-    ]
+do {
+    var config = Config.default()
+    var env = try Environment.detect()
+    var services = Services.default()
+    
+    try App.configure(&config, &env, &services)
+    
+    let app = try Application(config: config, environment: env, services: services)
+    
+    try App.boot(app)
+    
+    let maxFrameLength: Int = 1 << 19
+    let worker = MultiThreadedEventLoopGroup(numberOfThreads: 6)
+    
+    let ws = try HTTPClient.webSocket(scheme: .wss, hostname: "ws-feed.pro.coinbase.com", maxFrameSize: maxFrameLength, on: worker).wait()
+
+    ws.onText { (ws, text) in
+        print("REC: ", text)
+    }
+    
+    ws.onError { (ws, error) in
+        print("Error: ", error)
+    }
+    
+//    TODO: Instead of passing the hardcoded string, figure out how to encode this Subscribe type and send that
+//    let s = Subscribe()
+//    let data = try JSONEncoder().encode(s)
+    
+    ws.send(text: """
+        {"type": "subscribe", "channels" : ["level2"], "product_ids" : ["BTC-USD"]}
+    """)
+    
+    try app.run()
+} catch {
+    print("Top Level Error: \(error)")
+    exit(1)
 }
-
-
-let worker = MultiThreadedEventLoopGroup(numberOfThreads: 2)
-
-let coinBaseWS = try HTTPClient.webSocket(
-    scheme: HTTPScheme.wss,
-    hostname: "ws-feed.pro.coinbase.com",
-    on: worker
-).wait()
-
-let s = Subscribe()
-let jsonData = try JSONEncoder().encode(s)
-let rawStr = String(bytes: jsonData, encoding: .utf8)
-
-coinBaseWS.send(String(rawStr!))
-
-
-coinBaseWS.onText({ws, text in
-    print("coinBase REC: ", text)
-})
-
-coinBaseWS.onBinary({ws, text in
-    print(text)
-})
-
-
-try app(.detect()).run()
